efa59c031bf691da6429f561e03ecfd2dd23c92b
Aditya Nalawade <157732130+Adiiiicodes@users.noreply.github.com>
2024-09-26 22:18:52 +0530

    Add files via upload
    This is an Implementation of Linear Search algorithm which returns the indices of the element which is searched and present in the list .

---
+++ LinearSearchAlgo.py
@@ -0,0 +1,13 @@
+def lsa(x,li,y):
+    for i in range(x):
+        if li[i]==y :
+            print(f'The element you are searchng is found at index {i}')
+
+x=int(input('Enter the size of the list : '))
+li=[]
+for i in range(x):
+    element=int(input(f'Enter the element at index {i} : '))
+    li.append(element)
+
+y=int(input('Enter the Element to be searched : '))
+lsa(x,li,y)
