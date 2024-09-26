3e4ba3f6322b8b5a49693803bb110fb5f1ee83b9
Aditya Nalawade <157732130+Adiiiicodes@users.noreply.github.com>
2024-09-26 22:25:32 +0530

    Update LinearSearchAlgo.py

--- LinearSearchAlgo.py
+++ LinearSearchAlgo.py
@@ -2,6 +2,8 @@ def lsa(x,li,y):
     for i in range(x):
         if li[i]==y :
             print(f'The element you are searchng is found at index {i}')
+        else :
+            print('No match found')
 
 x=int(input('Enter the size of the list : '))
 li=[]
